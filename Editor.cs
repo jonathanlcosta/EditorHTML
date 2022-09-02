b5e9e0acc8ca5db2e1f800be19d873cc737df69a
Jonathan Costa <111704132+jonathanlcosta@users.noreply.github.com>
2022-09-02 17:35:43 -0300

    Add files via upload

---
+++ Editor.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show(){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("------------");
+            Start();
+
+        }
+        public static void Start(){}
+
+    }
+}
